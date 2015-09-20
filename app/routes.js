d8d6a101038227a55d755e22dbc4ec253167d13f
Matt <matt.voget@gmail.com>
2015-09-19 19:46:33 -0600

    test

--- app/routes.js
+++ app/routes.js
@@ -192,7 +192,7 @@ module.exports = function(app) {
 	app.use('/admin',adminApiRoutes);
 
 	// Application Routes ======================
-	app.get('*', function(req,res){
+	app.get('/', function(req,res){
 		res.sendfile('./public/views/index.html');
 	});
 
