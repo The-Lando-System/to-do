7419e541b3a123c49671a79bbaca4d076b8e89e1
Matt <matt.voget@gmail.com>
2015-10-18 21:50:35 -0600

    favicon

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var favicon = require('serve-favicon');
 
 // Configuration ======================
 var devConfig = false;
@@ -27,6 +28,7 @@ app.use(bodyParser.urlencoded({ 'extended':'true' }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type:'application/vnd.api+json' }));
 app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 // Routes ==============================
 require('./app/adminRoutes')(app);
